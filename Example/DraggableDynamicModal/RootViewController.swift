c3255caf6bee2660e031357e90b0df633d34e052
Ali ABBAS <Ali@MacBook-Pro-de-Ali-ABBAS.local>
2017-07-13 17:52:46 +0200

    - Improving exemple with modal view controller. - Adding an assert to check if the child view controller conform to the protocol : ModalParentViewControllerProtocol

---
+++ Example/DraggableDynamicModal/RootViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RootViewController.swift
+//  DraggableDynamicModal
+//
+//  Created by Ali ABBAS on 13/07/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
